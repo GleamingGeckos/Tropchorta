54595bd5f086cfe9315c49ecaeef9fa90ed15187
Klaudia <alice4book@gmail.com>
2025-02-24 00:01:43 +0100

    fix: Enemy rotates only around Y

--- Tropchorta/Assets/Scripts/Enemies/EnemyMovement.cs
+++ Tropchorta/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -6,9 +6,11 @@ public class EnemyMovement : MonoBehaviour
     {
         if (obj == null) return; // Safety check  
 
+        
         Vector3 direction = (obj.transform.position - transform.position).normalized; // Get direction  
         Quaternion lookRotation = Quaternion.LookRotation(direction); // Calculate rotation  
 
-        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f); // Smooth rotation 
+        Quaternion newRotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5.0f); // Smooth rotation 
+        transform.rotation = Quaternion.Euler(0.0f, newRotation.eulerAngles.y, 0.0f);
     }
 }
