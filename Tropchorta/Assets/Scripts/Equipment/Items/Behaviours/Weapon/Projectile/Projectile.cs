9bd6f1c13dd69f9f775e29cf5e41d22babb2483a
Klaudia <alice4book@gmail.com>
2025-05-21 23:42:46 +0200

    feat: Magic Projectile

--- Tropchorta/Assets/Scripts/Equipment/Items/Behaviours/Weapon/Projectile/Projectile.cs
+++ Tropchorta/Assets/Scripts/Equipment/Items/Behaviours/Weapon/Projectile/Projectile.cs
@@ -10,16 +10,33 @@ public class Projectile : MonoBehaviour
     public string targetTag = "Enemy";
 
     float currentSpeed;
+    [SerializeField] private float motionScale = 1f;
+    [SerializeField] private float motionSpeed = 1f;
+
+    private float motionTime;
+
+    public AnimationCurve motionCurveX;
+    public AnimationCurve motionCurveY;
+    public AnimationCurve motionCurveZ;
 
     void Start()
     {
+        currentSpeed = speed;
         Destroy(gameObject, lifetime);
     }
 
     void Update()
     {
+        motionTime += Time.deltaTime * motionSpeed;
+
+        float offsetX = motionCurveX.Evaluate((motionTime)%1) * motionScale;
+        float offsetY = motionCurveY.Evaluate((motionTime)%1) * motionScale;
+        float offsetZ = motionCurveZ.Evaluate((motionTime)%1) * motionScale;
+
+        Vector3 lateralOffset = Vector3.right * offsetX + Vector3.up * offsetY + Vector3.forward * offsetZ;
+
         currentSpeed = Mathf.Max(currentSpeed - slowdownRate * Time.deltaTime, minSpeed);
-        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime + lateralOffset * Time.deltaTime);
     }
 
     void OnTriggerEnter(Collider other)
