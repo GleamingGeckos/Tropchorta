27fef968c8eb8d138c951e00444804285baea7e6
Klaudia <alice4book@gmail.com>
2025-05-28 00:56:21 +0200

    Movement with keys not mouse

--- Tropchorta/Assets/Scripts/Player/PlayerMovement.cs
+++ Tropchorta/Assets/Scripts/Player/PlayerMovement.cs
@@ -93,6 +93,7 @@ public class PlayerMovement : MonoBehaviour
         if (lerpedMove.sqrMagnitude > 0.1f) // sqrt so with normal values (>1) it should always be greater than speed
         {
             PlayFootstepsSound();
+            modelRootTransform.forward = move.normalized;
         }
         else
         {
@@ -100,14 +101,14 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // Rotate the player to face the mouse position
-        if (mousePosition != Vector2.zero)
-        {
-            Vector3 lookDirection = new Vector3(mousePosition.x, 0, mousePosition.y);
-            if (lookDirection != Vector3.zero)
-            {
-                modelRootTransform.forward = lookDirection;
-            }
-        }
+        //if (mousePosition != Vector2.zero)
+        //{
+        //    Vector3 lookDirection = new Vector3(mousePosition.x, 0, mousePosition.y);
+        //    if (lookDirection != Vector3.zero)
+        //    {
+        //        modelRootTransform.forward = lookDirection;
+        //    }
+        //}
     }
 
 
